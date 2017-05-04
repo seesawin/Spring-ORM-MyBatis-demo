ca4fda3b0a31c5643eef74a17a92fade86193223
seesawin <seesawin@gmail.com>
2017-05-04 18:33:57 +0800

    update

--- src/main/java/ts/action/QueryAction.java
+++ src/main/java/ts/action/QueryAction.java
@@ -3,88 +3,78 @@ package ts.action;
 import java.util.List;
 
 import org.apache.commons.lang.builder.ToStringBuilder;
-import org.apache.ibatis.session.SqlSession;
-import org.apache.ibatis.session.defaults.DefaultSqlSessionFactory;
+import org.mybatis.spring.SqlSessionTemplate;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.jdbc.datasource.DataSourceTransactionManager;
+import org.springframework.transaction.TransactionDefinition;
+import org.springframework.transaction.TransactionStatus;
+import org.springframework.transaction.support.DefaultTransactionDefinition;
 
 import ts.mapper.CityMapper;
-import ts.mapper.CountryMapper;
-import ts.mapper.CountrylanguageMapper;
 import ts.persistence.City;
 import ts.persistence.CityExample;
-import ts.persistence.Country;
-import ts.persistence.CountryExample;
-import ts.persistence.Countrylanguage;
-import ts.persistence.CountrylanguageExample;
 
 public class QueryAction {
 	
-	private ApplicationContext context = null;
-	private DefaultSqlSessionFactory sessionFactory = null;
+	public SqlSessionTemplate sqlSession = null;
 	
-	public QueryAction() {
-		if(context == null) {
-			context = new ClassPathXmlApplicationContext(new String[] { "classpath:/spring/application-context.xml" });
+	public DataSourceTransactionManager txManager = null;
+	
+	public void setSqlSession(SqlSessionTemplate sqlSession) {
+		this.sqlSession = sqlSession;
+	}
+
+	public void setTxManager(DataSourceTransactionManager txManager) {
+		this.txManager = txManager;
+	}
+
+	public void inserCity(String name, String code, String district, Integer pooulation) throws Exception {
+		TransactionDefinition transactionDefinition = new DefaultTransactionDefinition();
+		TransactionStatus transactionStatus = txManager.getTransaction(transactionDefinition);
+		
+		try {
+			CityMapper city = sqlSession.getMapper(CityMapper.class);
+			City example = new City();
+			example.setName(name);
+			example.setCountrycode(code);
+			example.setDistrict(district);
+			example.setPopulation(pooulation);
+			city.insert(example);
+			
+			if(1 == 1) {
+				throw new Exception("Transaction ฟ๙ป~ด๚ธี!");
+			}
+			
+		} catch (Exception e) {
+			txManager.rollback(transactionStatus);
+			throw e;
 		}
-		sessionFactory = (DefaultSqlSessionFactory) context.getBean("sqlSessionFactory");
-	} 
+		
+		txManager.commit(transactionStatus);
+	}
 	
-	public void searchCity() {
-		System.out.println("======= searchCity =======");
-		SqlSession session = sessionFactory.openSession();
+	public void searchCityByExample(String name) {
+		System.out.println("======= searchCityByExample =======");
 		
-		CityMapper city = session.getMapper(CityMapper.class);
+		CityMapper city = sqlSession.getMapper(CityMapper.class);
+		CityExample example = new CityExample();
+		example.createCriteria().andNameEqualTo(name);
+		List<City> list = city.selectByExample(example);
 		
-		List<City> list = city.selectByExample(new CityExample());
 		for(int i = 0; i < list.size(); i++) {
 			System.out.println(ToStringBuilder.reflectionToString(list.get(i)));
-			if(i == 5) {
-				break;
-			}
 		}
 		
-		session.close();
 	}
 	
-	public void searchCountry() {
-		System.out.println("======= searchCountry =======");
-		SqlSession session = sessionFactory.openSession();
+	public static void main(String[] args) throws Exception {
+		ApplicationContext context = new ClassPathXmlApplicationContext(new String[] { "classpath:/spring/application-context.xml" });
+		QueryAction query = (QueryAction) context.getBean("QueryAction");
 		
-		CountryMapper country = session.getMapper(CountryMapper.class);
-		
-		List<Country> list = country.selectByExample(new CountryExample());
-		for(int i = 0; i < list.size(); i++) {
-			System.out.println(ToStringBuilder.reflectionToString(list.get(i)));
-			if(i == 5) {
-				break;
-			}
-		}	
-		session.close();
-	}
-	
-	public void searchCountryLanguage() {
-		System.out.println("======= searchCountryLanguage =======");
-		SqlSession session = sessionFactory.openSession();
-		
-		CountrylanguageMapper language = session.getMapper(CountrylanguageMapper.class);
-		
-		List<Countrylanguage> list = language.selectByExample(new CountrylanguageExample());
-		for(int i = 0; i < list.size(); i++) {
-			System.out.println(ToStringBuilder.reflectionToString(list.get(i)));
-			if(i == 5) {
-				break;
-			}
-		}	
-		
-		session.close();
-	}
-	
-	public static void main(String[] args) {
-		QueryAction query = new QueryAction();
-		query.searchCity();
-		query.searchCountry();
-		query.searchCountryLanguage();
+		String name = "Taipei7";
+//		query.inserCity(name, "TWN", "GENM", 56565);
+		query.searchCityByExample(name);
 	}
 
 }
